ca8636230a350cff4ee50bea539c22830d9e01ea
lgbouma <bouma.luke@gmail.com>
2020-05-05 16:49:41 -0700

    add drivers.plot_quicklooklc

---
+++ drivers/plot_quicklooklc.py
@@ -0,0 +1,16 @@
+import os
+from glob import glob
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+from timmy import DATADIR, RESULTSDIR
+from timmy.plotting import plot_quicklooklc
+
+overwrite = 1
+
+outdir = os.path.join(RESULTSDIR, 'hacky_plots')
+
+plot_quicklooklc(outdir, yval='SAP_FLUX', overwrite=overwrite)
+plot_quicklooklc(outdir, yval='PDCSAP_FLUX', overwrite=overwrite)
+plot_quicklooklc(outdir, yval='IRM1', provenance='cdips', overwrite=overwrite)
+plot_quicklooklc(outdir, yval='PCA1', provenance='cdips', overwrite=overwrite)
