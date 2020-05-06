136d07ab9bd92e1f9ea9c765ad2048ee651603d9
lgbouma <bouma.luke@gmail.com>
2020-05-06 14:23:56 -0700

    update priors

--- timmy/priors.py
+++ timmy/priors.py
@@ -1,6 +1,7 @@
+import numpy as np
 from collections import OrderedDict
 
-def initialize_prior_d(x_obs, modelcomponents):
+def initialize_prior_d(modelcomponents):
 
     # SPOC multisector report
     P_orb = 8.32467 # +/- 4e-4
@@ -10,9 +11,9 @@ def initialize_prior_d(x_obs, modelcomponents):
     # Visual inspection
     P_rot = 3.3 # +/- 1 (by eye)
     t0_rot = None # idk, whatever phase. it's not even a model parameter
-    amp = 1.5e-2 # primary mode amplitude. peak to peak is a bit over 2%
+    amp = 1e3*1.5e-2 # primary mode amplitude [ppt]. peak to peak is a bit over 2%
     amp_mix = 0.5 # between 0 and 1
-    log_Q0 = np.log(1e2) # Q of secondary oscillation. wide prior
+    log_Q0 = np.log(1e1) # Q of secondary oscillation. wide prior
     log_deltaQ = np.log(1e1) # primary mode gets higher quality
 
     prior_d = OrderedDict()
@@ -24,11 +25,11 @@ def initialize_prior_d(x_obs, modelcomponents):
             prior_d['t0'] = t0_orb
             prior_d['r'] = rp_rs
             prior_d['b'] = 0.5  # initialize for broad prior
-            prior_d['u'] = [0.30,0.35] # 0.39Msun, 1.39Rsun -> logg=3.74. Teff 3500K -> Claret18
-            prior_d['mean'] = 1
+            prior_d['u'] = [0.3189,0.2278] # Teff 6300K, logg 4.50 (Claret+18)
+            prior_d['mean'] = 0
 
         if 'gp' in modelcomponent:
-            prior_d['period'] = P_rot
+            prior_d['P_rot'] = P_rot
             prior_d['amp'] = amp
             prior_d['mix'] = amp_mix
             prior_d['log_Q0'] = log_Q0
