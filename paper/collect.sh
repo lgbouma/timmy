3e4c22a6710979ae756b46cb3a99d3de25570398
lgbouma <bouma.luke@gmail.com>
2020-05-07 15:37:29 -0700

    init commit paper

---
+++ paper/collect.sh
@@ -0,0 +1,27 @@
+# Usage: copy in figures for the paper
+
+pdir=../results/paper_plots/
+
+cp ${pdir}raw_zoom_spoc_PDCSAP_FLUX.pdf f1.pdf
+
+# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap_ii.pdf f2.pdf
+# 
+# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_phasefoldmap.pdf f3.pdf
+# 
+
+cp ${pdir}scene.pdf f3.pdf
+
+cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f4.pdf
+
+cp ${pdir}toi_837_sed.png f6.png
+
+# 
+# cp ../results/cluster_membership/hr.pdf f5a.pdf
+# cp ../results/cluster_membership/astrometric_excess.pdf f5b.pdf
+# 
+# cp ../results/ephemeris/O_minus_C.pdf f6.pdf
+# 
+# cp ../results/brethren/brethren.pdf f7.pdf
+# 
+# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_cornerplot.png f6.png
+# pngquant f6.png --output f6_comp.png
