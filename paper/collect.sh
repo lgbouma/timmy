7447a3f9e66c290e37b71c07f2fa9dbc1fcbdab1
lgbouma <bouma.luke@gmail.com>
2020-05-08 11:26:12 -0700

    update paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -1,15 +1,11 @@
 # Usage: copy in figures for the paper
 
+rdir=/Users/luke/Dropbox/proj/timmy/results/
 pdir=../results/paper_plots/
 
 cp ${pdir}raw_zoom_spoc_PDCSAP_FLUX.pdf f1.pdf
 
-cp /Users/luke/Dropbox/proj/timmy/results/TOI_837_transit_phot_results/20200506/TOI_837_transit_phaseplot.pdf f2.pdf
-
-# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_splitsignalmap_ii.pdf f2.pdf
-# 
-# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_phasefoldmap.pdf f3.pdf
-# 
+cp ${rdir}TOI_837_transit_phot_results/20200506/TOI_837_transit_phaseplot.pdf f2.pdf
 
 cp ${pdir}scene.pdf f3.pdf
 
@@ -17,13 +13,4 @@ cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f4.p
 
 cp ${pdir}toi_837_sed.png f6.png
 
-# 
-# cp ../results/cluster_membership/hr.pdf f5a.pdf
-# cp ../results/cluster_membership/astrometric_excess.pdf f5b.pdf
-# 
-# cp ../results/ephemeris/O_minus_C.pdf f6.pdf
-# 
-# cp ../results/brethren/brethren.pdf f7.pdf
-# 
-# cp ${pdir}PTFO_8-8695_transit_2sincosPorb_2sincosProt_cornerplot.png f6.png
-# pngquant f6.png --output f6_comp.png
+cp ${rdir}cluster_membership/hr.pdf f7.pdf
