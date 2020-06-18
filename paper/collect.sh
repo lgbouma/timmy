7a1b1b7f1484251c602f03b278058a677f281e33
lgbouma <bouma.luke@gmail.com>
2020-06-18 11:50:09 -0700

    restructure paper

--- paper/collect.sh
+++ paper/collect.sh
@@ -5,22 +5,24 @@ pdir=../results/paper_plots/
 
 cp ${pdir}raw_zoom_spoc_PDCSAP_FLUX.pdf f1.pdf
 
-cp ${rdir}TOI_837_transit_phot_results/20200518/TOI_837_transit_phaseplot.pdf f2.pdf
+cp ${rdir}TOI_837_alltransit_quad_phot_results/20200617/TOI_837_*_phaseplot.pdf f2a.pdf
 
-cp ${pdir}scene.pdf f3.pdf
+cp ${rdir}TOI_837_alltransit_quad_phot_results/20200617/TOI_837_*_grounddepth.pdf f2b.pdf
 
-cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f4.pdf
+cp ${rdir}radvel_fitting/20200525_simple_planet/TOI837_rv_multipanel.pdf f3.pdf
 
-cp ${rdir}lithium/lithium.pdf f5.pdf
+cp ${rdir}fpscenarios/fpscenarios.pdf f4.pdf
 
-cp ${rdir}rotation/rotation.pdf f9.pdf
+cp ${pdir}scene.pdf f5.pdf
 
-cp ${pdir}toi_837_sed.png f6.png
+cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f6.pdf
 
 cp ${rdir}cluster_membership/hr.pdf f7.pdf
 
 cp ${rdir}cluster_membership/full_kinematics.pdf f8.pdf
 
-cp ${rdir}fpscenarios/fpscenarios.pdf f10.pdf
+cp ${rdir}lithium/lithium.pdf f9a.pdf
 
-cp ${rdir}TOI_837_transit_phot_results/20200518/TOI_837_transit_grounddepth.pdf f11.pdf
+cp ${rdir}rotation/rotation.pdf f9b.pdf
+
+# cp ${pdir}toi_837_sed.png f6.png
