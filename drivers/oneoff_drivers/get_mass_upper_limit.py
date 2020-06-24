2e7e333aa1f5e461f9786235f6eefb9e178adf41
lgbouma <bouma.luke@gmail.com>
2020-06-24 15:42:12 -0700

    add one-off rv mass upper limit script

---
+++ drivers/oneoff_drivers/get_mass_upper_limit.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd, numpy as np
+from astrobase.lcmath import find_lc_timegroups
+from numpy import array as nparr
+from astropy import units as u
+
+from timmy.paths import DATADIR, RESULTSDIR
+
+datestr = '20200624'
+postpath = os.path.join(RESULTSDIR, 'radvel_fitting',
+                        f'{datestr}_simple_planet',
+                        'TOI837_derived.csv.tar.bz2')
+
+df = pd.read_csv(postpath)
+
+cutoff = 99.7 # 3-sigma
+
+val = np.percentile(nparr(df.mpsini1), cutoff)*u.Mearth
+
+print(f'3sigma (99.7th percentile): {val}')
+print(f'3sigma (99.7th percentile): {val.to(u.Mjup)}')
