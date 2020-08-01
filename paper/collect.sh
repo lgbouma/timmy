722874ad1fc1ac185f54974a6ef768b57c99c17f
lgbouma <bouma.luke@gmail.com>
2020-08-01 10:08:24 -0700

    figure renaming

--- paper/collect.sh
+++ paper/collect.sh
@@ -3,27 +3,27 @@
 rdir=/Users/luke/Dropbox/proj/timmy/results/
 pdir=../results/paper_plots/
 
-cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_fitindiv.pdf f1.pdf
+cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_fitindiv.pdf f1a.pdf
 
-cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_phaseplot.pdf f2a.pdf
+cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_phaseplot.pdf f1b.pdf
 
-cp ${rdir}TOI_837_allindivtransit_phot_results/20200711/TOI_837_*_grounddepth.pdf f2b.pdf
+cp ${pdir}scene.pdf f2.pdf
 
-cp ${pdir}scene.pdf f5.pdf
+cp ${rdir}TOI_837_allindivtransit_phot_results/20200711/TOI_837_*_grounddepth.pdf f3.pdf
 
-cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f6.pdf
+cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f4.pdf
 
-cp ${rdir}radvel_fitting/20200624_simple_planet/TOI837_rvlimit_multipanel.pdf f3.pdf
+cp ${rdir}radvel_fitting/20200624_simple_planet/TOI837_rvlimit_multipanel.pdf f5.pdf
 
-cp ${rdir}fpscenarios/fpscenarios.pdf f4.pdf
+cp ${rdir}fpscenarios/fpscenarios.pdf f6.pdf
 
 cp ${rdir}cluster_membership/hr_withiso_Bp_m_Rp.pdf f7.pdf
 
 cp ${rdir}cluster_membership/full_kinematics.pdf f8.pdf
 
-cp ${rdir}lithium/lithium.pdf f9a.pdf
+cp ${rdir}rotation/rotation.pdf f9a.pdf
 
-cp ${rdir}rotation/rotation.pdf f9b.pdf
+cp ${rdir}lithium/lithium.pdf f9b.pdf
 
 cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_subsetcorner.pdf f10.pdf
 
