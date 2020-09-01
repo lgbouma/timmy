7edecec9b70503385c810a28f2438cf40a4b606f
lgbouma <bouma.luke@gmail.com>
2020-09-01 14:22:26 -0700

    update figures

--- paper/collect.sh
+++ paper/collect.sh
@@ -3,21 +3,21 @@
 rdir=/Users/luke/Dropbox/proj/timmy/results/
 pdir=../results/paper_plots/
 
-cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_fitindiv.pdf f1a.pdf
+cp ${rdir}TOI_837_allindivtransit_phot_results/20200828/TOI_837_*_fitindiv.pdf f1a.pdf
 
-cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_phaseplot.pdf f1b.pdf
+cp ${rdir}TOI_837_allindivtransit_phot_results/20200828/TOI_837_*_phaseplot.pdf f1b.pdf
 
 cp ${pdir}scene.pdf f2.pdf
 
 cp /Users/luke/Dropbox/proj/timmy/data/speckle/TIC460205581I-cz20190714.pdf f3.pdf
 
-cp ${rdir}TOI_837_allindivtransit_phot_results/20200711/TOI_837_*_grounddepth.pdf f4.pdf
+cp ${rdir}TOI_837_allindivtransit_phot_results/20200828/TOI_837_*_grounddepth.pdf f4.pdf
 
 cp ${rdir}radvel_fitting/20200624_simple_planet/TOI837_rvlimit_multipanel.pdf f5.pdf
 
 cp ${rdir}fpscenarios/fpscenarios.pdf f6.pdf
 
-cp ${rdir}cluster_membership/hr_withiso_Bp_m_Rp.pdf f7.pdf
+cp ${rdir}cluster_membership/hr_mist_Bp_m_Rp.pdf f7.pdf
 
 cp ${rdir}cluster_membership/full_kinematics.pdf f8.pdf
 
@@ -25,10 +25,10 @@ cp ${rdir}rotation/rotation.pdf f9a.pdf
 
 cp ${rdir}lithium/lithium.pdf f9b.pdf
 
-cp ${rdir}TOI_837_tessindivtransit_phot_results/20200711/TOI_837_*_subsetcorner.pdf f10.pdf
+cp ${rdir}TOI_837_allindivtransit_phot_results/20200828/TOI_837_*_subsetcorner.pdf f10.pdf
 
-cp ${rdir}rp_vs_age_scatter/rp_vs_age_scatter_20200827_toi837.pdf f11a.pdf
+cp ${rdir}rp_vs_age_scatter/rp_vs_age_scatter_20200901_toi837.pdf f11a.pdf
 
-cp ${rdir}rp_vs_period_scatter/rp_vs_period_scatter_20200827_toi837_nolegend.pdf f11b.pdf
+cp ${rdir}rp_vs_period_scatter/rp_vs_period_scatter_20200901_toi837_nolegend.pdf f11b.pdf
 
 # cp ${pdir}toi_837_sed.png f6.png
